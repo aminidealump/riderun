8fede9356678e102ed3e1389845f6ebe41dcd749
aminidealump <amin@idealump.com>
2025-07-03 17:02:00 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_54ndwuvBX', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1l5954ujajnotpn2fc9cvvnv5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-2_EGL5pswBA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1c41vramlsnohtninehlm9cqcu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
